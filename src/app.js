2c032ccbfc736ac78ca2391c47b12a72bd3a5d56
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-10-26 11:06:15 +0500

    commented close campaign cron

--- src/app.js
+++ src/app.js
@@ -43,78 +43,78 @@ app.use((req, res, next) => {
   next(new ApiError(404, "Not found"));
 });
 
-var closeCampaigns = new cron(
-  "* * * * * *",
-  async function () {
-    const campagins = await Project.find({
-      status: "approved",
-      isActive: true,
-      // createdAt: { $lt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
-    }).populate("investment");
+// var closeCampaigns = new cron(
+//   "* * * * * *",
+//   async function () {
+//     const campagins = await Project.find({
+//       status: "approved",
+//       isActive: true,
+//       // createdAt: { $lt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000) },
+//     }).populate("investment");
 
-    const today = new Date();
-    const todayYear = today.getFullYear();
-    const todayMonth = today.getMonth();
-    const todayDate = today.getDate();
+//     const today = new Date();
+//     const todayYear = today.getFullYear();
+//     const todayMonth = today.getMonth();
+//     const todayDate = today.getDate();
 
-    Promise.all(
-      campagins.map(async (campagin) => {
-        let date = new Date(campagin.createdAt);
-        date.setDate(date.getDate() + 30);
-        let year = date.getFullYear();
-        let month = date.getMonth();
-        let day = date.getDate();
-        if (todayYear === year && todayMonth === month && todayDate === day) {
-          let totalInvestment = 0;
-          campagin.investment.forEach((investment) => {
-            totalInvestment += investment.amount;
-          });
-          const completedFundingGoalPercentage = Math.round(
-            (totalInvestment / campagin.fundingGoal) * 100
-          );
-          if (completedFundingGoalPercentage >= 80) {
-            await Project.findByIdAndUpdate(campagin._id, {
-              status: "completed",
-            });
-          } else {
-            // refund all the investments and close the campaign
-            Promise.all(
-              campagin.investment.map(async (investment) => {
-                const investor = await investorProfile.findOne({
-                  _id: investment.investor,
-                });
-                const payout = await stripe.payouts.create({
-                  amount: investment.amount * 100,
-                  currency: investment.currency,
-                  method: "standrd",
-                  destination: {
-                    iban: investor.iban,
-                  },
-                });
-                if (payout.status == "paid") {
-                  await Investment.findByIdAndUpdate(investment._id, {
-                    payout: payout,
-                    payoutAt: new Date(),
-                    payoutStatus: true,
-                  });
-                }
-                return payout;
-              })
-            ).then(async (payouts) => {
-              console.log(payouts);
-              await Project.findByIdAndUpdate(campagin._id, {
-                status: "closed",
-              });
-            });
-          }
-        }
-      })
-    );
-  },
-  null,
-  true,
-  "America/Los_Angeles"
-);
+//     Promise.all(
+//       campagins.map(async (campagin) => {
+//         let date = new Date(campagin.createdAt);
+//         date.setDate(date.getDate() + 30);
+//         let year = date.getFullYear();
+//         let month = date.getMonth();
+//         let day = date.getDate();
+//         if (todayYear === year && todayMonth === month && todayDate === day) {
+//           let totalInvestment = 0;
+//           campagin.investment.forEach((investment) => {
+//             totalInvestment += investment.amount;
+//           });
+//           const completedFundingGoalPercentage = Math.round(
+//             (totalInvestment / campagin.fundingGoal) * 100
+//           );
+//           if (completedFundingGoalPercentage >= 80) {
+//             await Project.findByIdAndUpdate(campagin._id, {
+//               status: "completed",
+//             });
+//           } else {
+//             // refund all the investments and close the campaign
+//             Promise.all(
+//               campagin.investment.map(async (investment) => {
+//                 const investor = await investorProfile.findOne({
+//                   _id: investment.investor,
+//                 });
+//                 const payout = await stripe.payouts.create({
+//                   amount: investment.amount * 100,
+//                   currency: investment.currency,
+//                   method: "standrd",
+//                   destination: {
+//                     iban: investor.iban,
+//                   },
+//                 });
+//                 if (payout.status == "paid") {
+//                   await Investment.findByIdAndUpdate(investment._id, {
+//                     payout: payout,
+//                     payoutAt: new Date(),
+//                     payoutStatus: true,
+//                   });
+//                 }
+//                 return payout;
+//               })
+//             ).then(async (payouts) => {
+//               console.log(payouts);
+//               await Project.findByIdAndUpdate(campagin._id, {
+//                 status: "closed",
+//               });
+//             });
+//           }
+//         }
+//       })
+//     );
+//   },
+//   null,
+//   true,
+//   "America/Los_Angeles"
+// );
 
 var fetchSocialData = new cron(
   "0 * * * * *",
